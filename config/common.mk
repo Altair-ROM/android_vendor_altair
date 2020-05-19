871f74f1898ca1cb8533d8a1372a59c208c78b98
Martin J. Pollard <martin0566@gmail.com>
2020-05-19 14:23:03 -0400

    Added Altair system permissions for ThemePicker

--- config/common.mk
+++ config/common.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/altair/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.lineageos.android.xml
 
+# Altair System Permission
+PRODUCT_COPY_FILES += \
+    vendor/altair/config/permissions/privapp-permissions-altair-system.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-altair-system.xml
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce

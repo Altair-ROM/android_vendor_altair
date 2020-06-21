4b9489570d6d921c303e18646fd20709eaf8b246
Martin J. Pollard <martin0566@gmail.com>
2020-06-20 22:51:55 -0400

    Rename "themes_*.mk" to "overlays_*.mk" [2/2]

--- config/common_full.mk
+++ config/common_full.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/altair/config/common_mobile.mk)
 PRODUCT_SIZE := full
 
 # Themes and Overlays
-$(call inherit-product, packages/overlays/Lineage/themes_full.mk)
+$(call inherit-product, packages/overlays/Lineage/overlays_full.mk)
 
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
--- config/common_mini.mk
+++ config/common_mini.mk
@@ -4,4 +4,4 @@ $(call inherit-product, vendor/altair/config/common_mobile.mk)
 PRODUCT_SIZE := mini
 
 # Themes and Overlays
-$(call inherit-product, packages/overlays/Lineage/themes_mini.mk)
+$(call inherit-product, packages/overlays/Lineage/overlays_mini.mk)
